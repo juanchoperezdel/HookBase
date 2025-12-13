9885ceefb526d880171bbd1927674318d499a23b
juanchoperezdel <perezdelgadojc@gmail.com>
2025-12-13 17:18:05 -0300

    Fix visual styles and tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'tailwindcss';
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
     "./*.{js,ts,jsx,tsx}"
   ],
   theme: {
